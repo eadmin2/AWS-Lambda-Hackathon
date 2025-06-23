dd2c63ee89a0afdafd28d812e6e49734ae775b76
Chris Lassiter <chrislassiter11@gmail.com>
2025-06-22 20:32:32 -0500

    cors update

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,6 +1,7 @@
 export const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Headers": 
+    "authorization, Authorization, x-client-info, apikey, content-type, Content-Type",
   "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
+  "Access-Control-Max-Age": "86400", // 24 hours
 };
